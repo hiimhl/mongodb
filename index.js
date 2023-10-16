13de956c4d8fd420710820b7d339bed50c46b8ed
hlin <gywjd5126@gmail.com>
2023-10-16 23:26:02 +0900

    body-parser, postman

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -16,6 +23,22 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  //client에서 회원 가입을 할 때 필요한 정보들을 가져오면
+  //해당 정보들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+
+  //save()는 몽고db 메소드
+  user
+    .save()
+    .then((userInfo) => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => res.json({ success: false, err }));
 });
 
 app.listen(port, () => {
--- models/User.js
+++ models/User.js
@@ -31,7 +31,7 @@ const userSchema = mongoose.Schema({
   },
 });
 
-const user = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
 
 // export
-module.exports = {};
+module.exports = { User };
